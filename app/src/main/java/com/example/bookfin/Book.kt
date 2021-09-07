4aa4623ed8cb6e13607869bb66f0d63b4fd9794c
Kener Barahona <barahonakener24@gmail.com>
2021-09-06 23:25:49 -0600

    cree mi clase Book y agrege el layout

---
+++ app/src/main/java/com/example/bookfin/Book.kt
@@ -0,0 +1,3 @@
+package com.example.bookfin
+
+class Book (val nombre_pretamista:String, val nombre_libro:String, val autor:String, val fecha_prestamo:Int, val fecha_entrega:Int, val numero_telefono:Int )
